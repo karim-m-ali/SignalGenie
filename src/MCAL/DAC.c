f055022b0c62352f9e1be54b215c803ed2d55c9a
Mahmoud Gamal Fareed <96448471+mgamalf@users.noreply.github.com>
2024-12-21 21:33:14 +0200

    Update DAC.c

--- src/MCAL/DAC.c
+++ src/MCAL/DAC.c
@@ -1,13 +1,21 @@
-// @author Your Name
+// @author Mahmoud Gamal
 // @date YYYY-MM-DD
 // @title Task Title
 
 #include "DAC.h"
+#include <lpc214x.h>
 
-// Function definitions go here
 
 
-void DAC_init(void) {}
+void DAC_init(void) {
+		 // Set bits 18:19 to 10 for AOUT (DAC)
+     PINSEL1 |= (1 << 18);
+		 // Clear bit 19
+     PINSEL1 &= ~(1 << 19);
 
-void DAC_write(uint16_t sample) {}
+}
+
+void DAC_write(uint16_t sample) {
+		 DACR = (sample & 0x3FF) << 6;
+}
 	
