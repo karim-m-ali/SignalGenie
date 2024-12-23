b52a0670ef04461a12466c89c804df55a5c0494d
El-Sherif Adel <116946567+SherifAdel2@users.noreply.github.com>
2024-12-23 02:02:20 +0200

    Update UART.c
    trust me this is the last version of UART

--- src/MCAL/UART.c
+++ src/MCAL/UART.c
@@ -1,69 +1,52 @@
-// @author Your Name
-// @date YYYY-MM-DD
-// @title Task Title
-
 #include "UART.h"
 #include <LPC214x.h> // Include LPC2148 header file
 
-// Function definitions go here
+
 
 void UART_init(void)
 {
-    // Step 1: Enable UART0 and GPIO peripheral
-    PINSEL0 |= 0x00000005; // Configure P0.0 as TXD0 and P0.1 as RXD0
-    
-    /* DLAB = 1, 1 stop bit, 8-bit character length */
-    U0LCR = 0x83;          // 8 bits, no parity, 1 stop bit, DLAB=1   
-    
-    U0DLM = 0x00;   /* For baud rate of 9600 with Pclk = 15MHz */
-    U0DLL = 0x61;            
-
-    // Step 3: Configure UART for normal operation
-    U0LCR = 0x03; // Disable DLAB (Divisor Latch Access Bit)
-    
-    // Step 4: Enable and reset FIFO
-    U0FCR = 0x07; // Enable FIFO, reset RX/TX FIFO
+	PINSEL0 = 0x5;  /* Select TxD for P0.0 and RxD for P0.1 */
+	U0LCR = 0x83; /* 8 bits, no Parity, 1 Stop bit | DLAB set to 1 */
+	U0DLL = 110;
+	U0DLM = 1;   
+	U0FDR = 0xF1; /* MULVAL=15(bits - 7:4) , DIVADDVAL=0(bits - 3:0)*/
+	U0LCR &= 0x0F; // Set DLAB=0 to lock MULVAL and DIVADDVAL
+	//BaudRate is now ~9600 and we are ready for UART communication! 
 }
+
 
 void UART_receiveLine(uint8_t *buffer, uint16_t bufferSize)
 {
-    uint16_t index = 0; // Initialize index for the buffer
-    uint8_t receivedChar; // Declare the variable at the beginning
+    uint16_t index = 0;
+    uint8_t receivedChar;
 
-    while (index < (bufferSize - 1)) { // Leave space for the null terminator
-        // Wait until data is available in the UART receive buffer
-        while (!(U0LSR & 0x01)) {
-            // Loop until RDR (Receiver Data Ready) bit is set
-        }
+    while (index < (bufferSize - 1))
+			{
+        while( (U0LSR & 0x01) == 0) {} // Wait until data is available
+        receivedChar = U0RBR;       // Read received character
 
-        // Read the received character from the UART data register
-        receivedChar = U0RBR;
-
-        // Check for end-of-line characters
-        if (receivedChar == '\n' || receivedChar == '\r') {
-            break; // Stop reading if end-of-line is reached
-        }
-
-        // Store the character in the buffer
+        if (receivedChar == '\n' || receivedChar == '\r')
+					{
+            break; // Stop on end-of-line characters
+					}
         buffer[index++] = receivedChar;
-    }
+     }
 
-    // Null-terminate the string
-    buffer[index] = '\0';
+    buffer[index] = '\0'; // Null-terminate the string
+		
 }
+
 
 void UART_sendLine(uint8_t *buffer, uint16_t stringLength)
 {
-    uint16_t i; // Declare the loop variable at the beginning
+    uint16_t i;
 
-    for (i = 0; i < stringLength; i++) {
-        // Wait until the UART transmitter holding register is empty
-        while (!(U0LSR & 0x20)) {
-            // Loop until the THRE (Transmit Holding Register Empty) bit is set
-        }
-
-        // Write the character to the UART transmit holding register
-        U0THR = buffer[i];
-    }
+    for (i = 0; i < stringLength; i++)
+	{
+		while( ( ((U0LSR & 0x20) == 0) && (U0LSR & 0x40) == 0) )  {} // Wait until the THRE flag is set
+		while (!(U0LSR & (1<<5))); // wait till the THR is empty
+		U0THR = buffer[i];         // Transmit the character
+   }
 }
 
+ 
